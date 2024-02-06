c19f93b09f23e4bd79ce8d650d124e02a258f060
Руслан Аитов <r_aitov@mail.ru>
2024-02-06 19:14:55 +0300

    task 24

--- task24/29/24.py
+++ task24/29/24.py
@@ -1,3 +1,10 @@
+def find_max_number(number, max_number):
+    if number and number[0] != '0' and number[-1] in '02468ACEGI':
+        if int(number, 20) > int(max_number, 20):
+            return number
+    return max_number
+
+
 f = open('24.txt')
 s = f.readline()
 number = ''
@@ -6,11 +13,7 @@ for c in s:
     if c in '0123456789ABCDEFGHIJ':
         number += c
     else:
-        if number and number[0] != '0' and number[-1] in '02468ACEGI':
-            if int(number, 20) > int(max_number, 20):
-                max_number = number
+        max_number = find_max_number(number, max_number)
         number = ''
-if number and number[0] != '0' and number[-1] in '02468ACEGI':
-    if int(number, 20) > int(max_number, 20):
-        max_number = number
+max_number = find_max_number(number, max_number)
 print(max_number)
