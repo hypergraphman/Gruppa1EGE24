84bb1f7549df147f0bf3fcb3704aceab7b2109e5
Руслан Аитов <r_aitov@mail.ru>
2024-03-05 18:04:35 +0300

    task 17

--- task17/6/6.py
+++ task17/6/6.py
@@ -8,9 +8,8 @@ for el in a:
         k += 1
 avg = s / k
 b = []
-for q1, q2, q3, q4 in zip(a, a[1:], a[2:], a[3:]):
-    t = [q1, q2, q3, q4]
-    if any(abs(x) % 15 == 11 for x in t) and (sum(x < avg for x in t) >= 2):
+for t in zip(a, a[1:], a[2:], a[3:]):
+    if any(abs(x) % 15 == 12 for x in t) and (sum(x < avg for x in t) >= 2):
         b.append(sum(t))
 
 print(len(b), max(b))
